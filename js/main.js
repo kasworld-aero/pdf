4d72e7373da939c91b9ebb84dc8aa1d13256d623
Sam Rose <samrose3@gmail.com>
2016-06-03 07:31:57 -0400

    refactored viewer api to utilize the PDFViewerApplication object

--- js/main.js
+++ js/main.js
@@ -72,7 +72,7 @@
         var $ctrl = this;
 
         $ctrl.searchQuery = '';
-        $ctrl.highlightAll = true;
+        $ctrl.highlightAll = false;
         $ctrl.keywords = ['nonlinear', 'discuss', 'duffing', 'equation'];
 
         $ctrl.selectKeyword = function(index) {
@@ -100,9 +100,9 @@
         bindings: {
             baseUrl: '<',
             file: '<',
-            find: '<',
-            highlight: '<',
             fullscreen: '<',
+            highlight: '<',
+            search: '<',
             onUpdate: '&'
         }
     };
@@ -112,37 +112,36 @@
     function pdfViewerCtrl($element, $sce, $q) {
         var $ctrl = this;
 
-        $ctrl.title = 'PDF Viewer';
-        $ctrl.height = $ctrl.height || '800px';
-        $ctrl.fullscreen = $ctrl.fullscreen || false;
-
+        // Attribute Properties with defaults
         $ctrl.baseUrl = $ctrl.baseUrl || '/pdfjs/web/viewer.html';
+        $ctrl.fullscreen = $ctrl.fullscreen || false;
         $ctrl.file = $ctrl.file || '/pdfjs/web/compressed.tracemonkey-pldi-09.pdf';
+        $ctrl.height = $ctrl.height || '800px';
+        $ctrl.highlight = $ctrl.highlight || false;
+        $ctrl.onUpdate = $ctrl.onUpdate || function() {};
+
+        // Computed Properties
         $ctrl.fileUrl = buildUrl();
 
+
+        // Update on data change
         $ctrl.$onChanges = function(changesObj) {
             console.log('PDF Viewer changed: ', changesObj);
             update(changesObj);
         };
-
-        function buildUrl() {
-            var url = $ctrl.baseUrl + '?file=' + encodeURIComponent($ctrl.file);
-            console.log(url);
-            return $sce.trustAsResourceUrl(url);
-        }
 
         function update(changes) {
             if (changes.file) {
                 $ctrl.fileUrl = buildUrl();
             }
 
-            if ($ctrl.viewer.ready) {
-                if (changes.find) {
-                    $ctrl.viewer.find($ctrl.find);
+            if ($ctrl.ready) {
+                if (changes.search) {
+                    $ctrl.find($ctrl.search);
                 }
 
                 if (changes.highlight) {
-                    $ctrl.viewer.highlightAll($ctrl.highlight);
+                    $ctrl.highlightAll($ctrl.highlight);
                 }
             }
 
@@ -151,67 +150,98 @@
             }
         }
 
-
+        // construct a TRUSTED URL for the iframe
+        function buildUrl() {
+            var url = $ctrl.baseUrl + '?file=' + encodeURIComponent($ctrl.file);
+            console.log(url);
+            return $sce.trustAsResourceUrl(url);
+        }
 
         // Viewer API
-        var viewer = {
+        var ctrlApi = {
+
+            // API Properties
             ready: false,
-            frame: null,
             findInput: null,
             findHighlightAll: null,
-            find_next: null,
-            find_previous: null,
+            findNext: null,
+            findPrevious: null,
+
+            // Attach DOM elements to API
             init: function() {
-                var viewer = this;
+                var ctrlApi = this;
                 var deferred = $q.defer();
-                viewer.frame = $element[0].firstChild;
-                viewer.frame.onload = function() {
+                var iframe = $element[0].firstChild;
+
+                iframe.onload = function() {
+                    $ctrl.PDFViewer = iframe.contentWindow.PDFViewerApplication;
+
                     // get document elements
-                    viewer.findInput = viewer.frame.contentWindow.document.getElementById('findInput');
+                    var content = iframe.contentWindow;
+                    var keys = [
+                        'findInput',
+                        'findHighlightAll',
+                        'findNext',
+                        'findPrevious'
+                    ];
+                    var elements = _.zipObject(keys, _.map(keys, function(id) {
+                        return content.document.getElementById(id);
+                    }));
+                    console.log(elements);
+                    _.assign(ctrlApi, elements);
 
-                    viewer.findHighlightAll = viewer.frame.contentWindow.document.getElementById('findHighlightAll');
-
-                    viewer.find_next = viewer.frame.contentWindow.document.getElementById('find_next');
-
-                    viewer.find_previous = viewer.frame.contentWindow.document.getElementById('find_previous');
-
-                    viewer.ready = true;
-                    deferred.resolve();
+                    deferred.resolve(true);
                 };
+
                 return deferred.promise;
             },
+
+            // Action Methods
             find: function(q) {
-                viewer.findInput.value = q;
-                viewer.fireEvent();
+                this.findInput.value = q;
+                $ctrl.PDFViewer.findBar.dispatchEvent('');
             },
             nextMatch: function() {
-                viewer.find_next.click();
+                $ctrl.PDFViewer.findBar.dispatchEvent('again', false);
             },
             prevMatch: function() {
-                viewer.find_previous.click();
+                this.findPrevious.click();
+                $ctrl.PDFViewer.findBar.dispatchEvent('again', true);
             },
             highlightAll: function(highlight) {
-                viewer.findHighlightAll.checked = highlight;
-                viewer.fireEvent();
-            },
-            fireEvent: function() {
-                var event = new Event('input');
-                viewer.findInput.dispatchEvent(event);
+                this.findHighlightAll.checked = highlight;
+                $ctrl.PDFViewer.findBar.dispatchEvent('highlightallchange');
             }
         };
-        $ctrl.viewer = viewer;
-        $ctrl.viewer.init().then();
-        // $ctrl.viewer.init().then(cycleFind);
+
+        // Attach API to the $ctrl
+        _.assign($ctrl, ctrlApi);
+        $ctrl.init().then(function(isReady) {
+            $ctrl.ready = isReady;
+
+            // DEMO
+            // findNext();
+            // cycleFind();
+        });
 
 
+
+        /* DEMO STUFF */
         function cycleFind() {
             var find = ['nonlinear', 'vibration', 'duffing'],
                 index = 0,
                 i = 0;
             setInterval(function() {
-                $ctrl.viewer.find(find[index]);
+                $ctrl.find(find[index]);
                 index = i++ % find.length;
                 console.log(find[index], index);
+            }, 2000);
+        }
+
+        function findNext() {
+            $ctrl.find('nonlinear');
+            setInterval(function() {
+                $ctrl.prevMatch();
             }, 2000);
         }
 
