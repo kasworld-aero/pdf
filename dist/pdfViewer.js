c5f4f1e97b663d224218cddb975197443f24d755
Sam Rose <samrose3@gmail.com>
2016-07-13 14:07:47 -0400

    removed browserify stuff from the pdfViewer module

--- dist/pdfViewer.js
+++ dist/pdfViewer.js
@@ -1,12 +1,8 @@
 (function() {
     'use strict';
 
-    require('angular');
-    require('oclazyload');
-
     var pdf_viewer = angular.module('pdf.viewer', ['oc.lazyLoad']);
 
-    module.exports = pdf_viewer;
 })();
 
 //HEAD 
--- pdfViewer/module.js
+++ pdfViewer/module.js
@@ -1,10 +1,6 @@
 (function() {
     'use strict';
 
-    require('angular');
-    require('oclazyload');
-
     var pdf_viewer = angular.module('pdf.viewer', ['oc.lazyLoad']);
 
-    module.exports = pdf_viewer;
 })();
